38c24fb4dd75761a6d80c72e692444013f21738d
Juliano Oliveira <oliveira.juliano@outlook,com>
2018-04-09 07:03:14 -0300

    Adicionando funções extra

--- app/src/main/java/juliano/oliveira/iddog/ApiServiceUtils.java
+++ app/src/main/java/juliano/oliveira/iddog/ApiServiceUtils.java
@@ -2,6 +2,7 @@ package juliano.oliveira.iddog;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -9,16 +10,28 @@ import android.widget.GridView;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiServiceUtils {
 
     private ApiServiceUtils() {}
-
+    private static Retrofit retrofit = null;
     public static final String BASE_URL = "https://iddog-api.now.sh";
+
+    public static Retrofit getClient(String baseUrl) {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(baseUrl)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
 
     public static IDDogService getAPIService() {
 
-        return RetrofitClient.getClient(BASE_URL).create(IDDogService.class);
+        return getClient(BASE_URL).create(IDDogService.class);
     }
 
     public static void GetDog( String category, String token, final Context ctx, final GridView grd) {
